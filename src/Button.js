ed4a85e6fc4b4552ec7638fbb64c4034a0f5c68b
jerin <jerin@cefalo.com>
2018-09-05 13:17:59 +0600

    Test openshift

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jerin on 8/1/18.
+ */
+import React, { Component } from 'react';
+import './Button.css'; // Tell Webpack that Button.js uses these styles
+
+class Button extends Component {
+    render() {
+        // You can use them as regular CSS styles
+        return <div className="Button" />;
+    }
+}
+
+export default Button;
